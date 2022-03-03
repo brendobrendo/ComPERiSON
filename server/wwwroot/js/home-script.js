ff21becc864978fe1fa31fd59bc82fe5b53a0faf
brendobrendo <brendan.smith903@gmail.com>
2022-03-03 08:32:54 -0800

    feature/success page button to comparisons

---
+++ server/wwwroot/js/home-script.js
@@ -0,0 +1,11 @@
+let test = document.getElementById("test");
+
+function changeImage(element) {
+    element.src = "/Users/brendansmith/Desktop/projects/Features/server/wwwroot/images/young_couple.jpeg"
+}
+
+function changeImageBack(element) {
+    element.src = "/Users/brendansmith/Desktop/projects/Features/server/wwwroot/images/gay_couple.jpeg"
+}
+
+// Want to change images on the Home Page. This may be deleted. Brendan
