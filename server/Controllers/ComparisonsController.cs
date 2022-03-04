a2c327af12992c6c8c55c321e621cffcbc1d3105
brendobrendo <brendan.smith903@gmail.com>
2022-03-04 08:43:27 -0800

    features/Candidate-and-CSess-ComContr

--- server/Controllers/ComparisonsController.cs
+++ server/Controllers/ComparisonsController.cs
@@ -36,7 +36,7 @@ namespace Features.Controllers
             db = context;
         }
 
-        [HttpGet("/comparisons/compare")]
+        [HttpPost("/comparisons/compare")]
         public IActionResult Compare()
         {
             if (!loggedIn)
@@ -44,44 +44,94 @@ namespace Features.Controllers
                 return RedirectToAction("Index");
             }
 
-            List<User> candidates = db.Users.Take(4).ToList();
-            return View("Compare", candidates);
+            // Get list of 4 random Users. These will be the candidates
+            // for the comparison
+            List<User> candidateUsers = db.Users.Take(4).ToList();
+            List<Candidate> candidates = new List<Candidate>();
+            
+            // Convert the Users to Candidates so we can track the score through
+            // the comparison process
+            foreach (User user in candidateUsers) 
+            {
+                Candidate candidate = new Candidate { UserId = user.UserId, Score = 0};
+                candidates.Add(candidate);
+            }
+
+            // Creating comparisonsession object to pass through the Comparison pages
+            // That will track candidate tallies and the bo selected by the user
+            ComparisonSession comparisonStage0 = new ComparisonSession { Candidates = candidates, WinningBoxNumber = 0};
+
+            return View("Compare", comparisonStage0);
         }
 
-        [HttpGet("/comparisons/compare2")]
-        public IActionResult Compare2()
+        [HttpPost("/comparisons/compare2")]
+        public IActionResult Compare2(ComparisonSession lastComparison)
+        {
+            if (!loggedIn)
+            {
+                return RedirectToAction("Index");
+            }
+            
+            int myNumber = 9;
+
+            // // generate random order of the input candidate list
+            // var random = new Random();
+
+            // // Shuffle the list of candidates
+            // for (int i = 0; i < candidates.Count; i++)
+            // {
+            //     int randomIndex = random.Next(candidates.Count);
+            //     User temp = candidates[i];
+            //     candidates[i] = candidates[randomIndex];
+            //     candidates[randomIndex] = temp;
+            // }
+
+            return View("Compare2");
+        }
+
+        [HttpPost("/comparisons/compare3")]
+        public IActionResult Compare3(List<User> candidates, string winningCandidateBox)
         {
             if (!loggedIn)
             {
                 return RedirectToAction("Index");
             }
 
-            List<User> candidates = db.Users.Take(4).ToList();
-            
             // generate random order of the input candidate list
             var random = new Random();
-            
-            
+
             // Shuffle the list of candidates
-            for (int i = 0; i<candidates.Count; i++)
+            for (int i = 0; i < candidates.Count; i++)
             {
                 int randomIndex = random.Next(candidates.Count);
                 User temp = candidates[i];
                 candidates[i] = candidates[randomIndex];
                 candidates[randomIndex] = temp;
             }
-            
-            return View("Compare2", candidates);
+
+            return View("Compare3", candidates);
         }
 
-        [HttpPost("/comparisons/compare3")]
-        public IActionResult Compare3()
+        [HttpGet("/comparisons/winning-candidate")]
+        public IActionResult Winner(List<User> candidates, int winningCandidateBox)
         {
             if (!loggedIn)
             {
                 return RedirectToAction("Index");
             }
-            return View();
+
+            var random = new Random();
+
+            // Shuffle the list of candidates
+            for (int i = 0; i < candidates.Count; i++)
+            {
+                int randomIndex = random.Next(candidates.Count);
+                User temp = candidates[i];
+                candidates[i] = candidates[randomIndex];
+                candidates[randomIndex] = temp;
+            }
+
+            return View("Winner", candidates);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
